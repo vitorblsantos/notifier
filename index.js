3deb3446eab0d56e70ac505966183e394c818e78
Vitor Barbosa de Lima Santos <visantos@sbt.com.br>
2022-06-22 17:05:34 -0300

    feat: integrando api com o mongodb

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+import mongoose from 'mongoose'
 import 'dotenv/config'
 
 import api from './app'
 
 const { API_PORT } = process.env || 8080
+const { MONGO_DB, MONGO_PARAMS, MONGO_PASSWORD, MONGO_URL, MONGO_USER } = process.env
 
-api.listen(API_PORT, () => console.log(`Api Running - Port: ${API_PORT}`))
+const url = `mongodb+srv://${encodeURIComponent(MONGO_USER)}:${encodeURIComponent(MONGO_PASSWORD)}@${MONGO_URL}/${MONGO_DB}?${MONGO_PARAMS}`
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+mongoose.connection.on('connected', () => {
+  api.listen(API_PORT, () => console.log(`Api running on port: ${API_PORT}`))
+})
+
+mongoose.connection.on('error', console.error.bind(console, 'Mongoose connection error:'))
